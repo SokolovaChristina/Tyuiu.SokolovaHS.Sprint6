5e86bf3f75863d9cbf423a2cd4a0b7284ba2da5b
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-12-04 03:52:13 +0500

    Task5

--- Tyuiu.SokolovaHS.Sprint6.Task5.V22.Lib/DataService.cs
+++ Tyuiu.SokolovaHS.Sprint6.Task5.V22.Lib/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Globalization;
 using tyuiu.cources.programming.interfaces.Sprint6;
 
@@ -10,19 +11,22 @@ namespace Tyuiu.SokolovaHS.Sprint6.Task5.V22.Lib
         public double[] LoadFromDataFile(string path)
         {
             string[] lines = File.ReadAllLines(path);
-            double[] numbers = new double[lines.Length];
+            List<double> numbersGreaterThan5 = new List<double>();
 
-            // Используем инвариантную культуру с точкой как разделителем
-            CultureInfo culture = CultureInfo.InvariantCulture;
+            CultureInfo culture = new CultureInfo("ru-RU");
 
             for (int i = 0; i < lines.Length; i++)
             {
-                // Заменяем запятую на точку
-                string line = lines[i].Replace(',', '.');
-                numbers[i] = Math.Round(double.Parse(line, culture), 3);
+                double number = Math.Round(double.Parse(lines[i], culture), 3);
+
+                // Добавляем только числа больше 5
+                if (number > 5)
+                {
+                    numbersGreaterThan5.Add(number);
+                }
             }
 
-            return numbers;
+            return numbersGreaterThan5.ToArray();
         }
     }
 }
