3c5ba3bb2ad81a89c6a34b1e3834dac6e4b5680c
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-12-04 01:09:23 +0500

    Task3

--- Tyuiu.SokolovaHS.Sprint6.Task3.V17.Lib/DataService.cs
+++ Tyuiu.SokolovaHS.Sprint6.Task3.V17.Lib/DataService.cs
@@ -11,40 +11,38 @@ namespace Tyuiu.SokolovaHS.Sprint6.Task3.V17.Lib
             // Создаем копию матрицы, чтобы не изменять оригинал
             int[,] resultMatrix = (int[,])matrix.Clone();
 
-            // Сортируем строки по 4-му столбцу (индекс 3) по возрастанию
-            // Используем сортировку пузырьком для строк
-            for (int i = 0; i < rows - 1; i++)
+            // Используем стабильную сортировку (сортировка вставками)
+            for (int i = 1; i < rows; i++)
             {
-                for (int j = 0; j < rows - i - 1; j++)
+                int[] currentRow = new int[cols];
+                int currentValue = resultMatrix[i, 3];
+
+                // Копируем текущую строку
+                for (int col = 0; col < cols; col++)
                 {
-                    // Сравниваем значения в 4-м столбце
-                    if (resultMatrix[j, 3] > resultMatrix[j + 1, 3])
+                    currentRow[col] = resultMatrix[i, col];
+                }
+
+                int j = i - 1;
+
+                // Сдвигаем строки с большими значениями вправо
+                while (j >= 0 && resultMatrix[j, 3] > currentValue)
+                {
+                    for (int col = 0; col < cols; col++)
                     {
-                        // Меняем строки местами
-                        SwapRows(resultMatrix, j, j + 1);
+                        resultMatrix[j + 1, col] = resultMatrix[j, col];
                     }
+                    j--;
+                }
+
+                // Вставляем текущую строку на правильное место
+                for (int col = 0; col < cols; col++)
+                {
+                    resultMatrix[j + 1, col] = currentRow[col];
                 }
             }
 
             return resultMatrix;
         }
-
-        /// <summary>
-        /// Меняет две строки матрицы местами
-        /// </summary>
-        private void SwapRows(int[,] matrix, int row1, int row2)
-        {
-            int cols = matrix.GetLength(1);
-
-            for (int col = 0; col < cols; col++)
-            {
-                int temp = matrix[row1, col];
-                matrix[row1, col] = matrix[row2, col];
-                matrix[row2, col] = temp;
-            }
-        }
     }
 }
-        
-    
-
